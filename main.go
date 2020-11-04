16361b73141285fd3407e1bd69d876ab52be51e2
Raazeshp96 <raazeshp96@gmail.com>
2020-11-05 00:36:34 +0545

    handiling block of channel

--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"os"
 )
 
 func main() {
@@ -13,15 +12,20 @@ func main() {
 		"https://www.facebook.com/",
 		"https://golang.org/",
 	}
+	c := make(chan string)
 	for _, link := range links {
-		go checkLink(link)
+		go checkLink(link, c)
+		fmt.Println(<-c)
 	}
 }
-func checkLink(link string) {
+
+func checkLink(link string, c chan string) {
 	_, err := http.Get(link)
 	if err != nil {
 		fmt.Println("Error: ", err)
-		os.Exit(1)
+		c <- "might be down"
+		return
 	}
 	fmt.Println(link, "is up")
+	c <- "yup its up"
 }
